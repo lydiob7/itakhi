5786bf3a53dcbf1670c52abc1a3f81d8289ffe17
lydiob7 <lydiob7@tutanota.com>
2023-11-30 17:35:56 -0300

    Update: next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // basePath: "/dev/website",
-    // output: "export",
+    output: "export",
     images: {
         remotePatterns: [
             {
